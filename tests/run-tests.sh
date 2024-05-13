465548cad2de4f89a59a4dc0f48106a2fd8e49b4
Saikrishna Arcot <sarcot@microsoft.com>
2024-05-13 14:01:11 -0700

    Enable debug logs for tests
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -6,9 +6,10 @@ TESTS="$3"
 RETRY=$4
 [ -z "$RETRY" ] && RETRY=1
 JUNITXML=$(echo "$TESTS" | cut -d "." -f1)_tr.xml
+LOGFILE=logs/$(echo "$TESTS" | cut -d "." -f1).log
 
 set -x
 for ((i=1; i<=$RETRY; i++)); do
     echo "Running the py test for tests: $TESTS, $i/$RETRY..."
-    py.test -v --force-flaky --junitxml="$JUNITXML" $PY_TEST_PARAMS --imgname="$IMAGE_NAME" $TESTS && break
+    py.test -v --force-flaky --junitxml="$JUNITXML" --log-file-level debug --log-file="$LOGFILE" $PY_TEST_PARAMS --imgname="$IMAGE_NAME" $TESTS && break
 done
